35a485727e7099e635cd12c76454e9911b8b230e
uarman29 <49767821+uarman29@users.noreply.github.com>
2019-10-24 18:37:58 -0400

    Update environment.prod.ts

--- shopping-project/src/environments/environment.prod.ts
+++ shopping-project/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyDISSTEBoWVBKGTmHKW0lkz0hFhZQaqkLU",
-    authDomain: "shopping-project-46130.firebaseapp.com",
-    databaseURL: "https://shopping-project-46130.firebaseio.com",
-    projectId: "shopping-project-46130",
-    storageBucket: "shopping-project-46130.appspot.com",
+    apiKey: "API_KEY",
+    authDomain: "DOMAIN",
+    databaseURL: "DATABASE",
+    projectId: "ID",
+    storageBucket: "STORAGE",
     messagingSenderId: "120395556208",
     appId: "1:120395556208:web:b770411c7b9cec57b01bb5",
     measurementId: "G-V4DJF4XJYE"
